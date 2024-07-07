3f4793715fe0f265f71afa77ec80a35b40405e93
maheshkotekey <mahesh.kotekey@gmail.com>
2024-07-07 11:05:46 +0530

    Method OverLoading Concept

---
+++ src/com/happylearning/overloadingConcept/java/Concept.java
@@ -0,0 +1,19 @@
+package com.happylearning.overloadingConcept.java;
+
+public class Concept {
+
+
+    public int learning(int e){
+        System.out.println(e);
+        return 0;
+
+    }
+    public String learning(int a,int b){
+        System.out.println(a+b);
+        return "ab";
+
+    }
+    public void learning(char d){
+        System.out.println(d);
+    }
+}
---
+++ src/com/happylearning/overloadingConcept/java/overloadingConcept.java
@@ -0,0 +1,18 @@
+package com.happylearning.overloadingConcept.java;
+
+public class overloadingConcept {
+
+
+    public static void main(String[] args){
+        Concept n = new Concept();
+
+        n.learning(1);
+        n.learning(2,4);
+        n.learning('f');
+
+
+    }
+
+
+
+}
